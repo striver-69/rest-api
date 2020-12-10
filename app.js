b6d1b48224ec45b4321f26f8735922919a0e24aa
Your Name <you@example.com>
2020-12-11 02:06:05 +0530

    parsing body and handling CORS error

--- app.js
+++ app.js
@@ -1,14 +1,32 @@
 const express=require('express')
 const app=express()
 const morgan=require('morgan')
+const bodyParser=require('body-parser')
 
 const productRoutes=require('./api/routes/product')
 const orderRoutes=require('./api/routes/orders')
 
 app.use(morgan('dev'))
+app.use(bodyParser.urlencoded({extended:false}))
+app.use(bodyParser.json())
+
+//allow CORS (Cross Origin Resourse sharing) security mechanisms
+app.use((req,res,next)=>{
+    res.header("Access-Control-Allow-Origin","*")
+    res.header(
+        "Access-Control-Allow-Headers",
+        "Origin,X-requested-With,Content-type,Accept,Authorization"
+    )
+    if(req.method === 'OPTIONS'){
+        res.header("Access-Control-Allow-Methods",'PUT,POST,PATCH,DELETE')
+        return res.status(200).json({})
+    }
+    next()
+})
+
 
 //routes which should handle requests
-app.use('/products',productRoutes)
+app.use('/products',productRoutes) 
 app.use('/orders',orderRoutes)
 
 //error if if above routes donot get requests
